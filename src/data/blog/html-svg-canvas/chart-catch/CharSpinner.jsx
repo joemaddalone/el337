3fd79c0ea9ff272c3bf45b751325a91b96cfa054
Joe Maddalone <joe.maddalone@gmail.com>
2025-12-16 13:39:42 -0600

    refactor: port more content

---
+++ src/data/blog/html-svg-canvas/chart-catch/CharSpinner.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+
+import { motion, useSpring } from "motion/react";
+import "./CharSpinner.css";
+
+const CharSpinner = ({ char, color }) => {
+  const chars = "0123456789".split("");
+
+  const top = useSpring(0, {
+    type: "spring",
+    stiffness: 500,
+    damping: 50
+  });
+
+  useEffect(() => {
+    top.set(chars.indexOf(char) * 55 * -1);
+  }, [char]);
+
+  return (
+    <div className="tiles">
+      <div className="tile">
+        <motion.div style={{ top: top }} className="char-holder">
+          {chars.map((c) => {
+            const active = c === char;
+            const tileClass = `tile-char ${active ? "active" : ""}`;
+            const style = { color };
+            return (
+              <div style={style} key={c} className={tileClass}>
+                {c}
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default CharSpinner;
